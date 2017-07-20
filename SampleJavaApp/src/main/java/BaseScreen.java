b9f8f7a62cbd94c0fe1adfbe3de1dab1f206d66a
Eugeniy Petrik <eugeniy.petrik@gmail.com>
2017-07-20 21:38:16 +0300

    Add methods for class BaseScreen

--- SampleJavaApp/src/main/java/BaseScreen.java
+++ SampleJavaApp/src/main/java/BaseScreen.java
@@ -1,2 +1,15 @@
 public abstract class BaseScreen {
+
+    String username;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    abstract public void getTitle();
+
+    public static int goBack(int number) {
+        return number;
+    }
+    
 }
