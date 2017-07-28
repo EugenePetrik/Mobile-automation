fe00664b03bace88e919133d1274e5ac800d7c59
Eugeniy Petrik <eugeniy.petrik@gmail.com>
2017-07-28 19:40:18 +0300

    Add capabilities

--- SampleAppTests/src/test/java/FirstTest.java
+++ SampleAppTests/src/test/java/FirstTest.java
@@ -1,2 +1,37 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 public class FirstTest {
+
+    public static AppiumDriver<MobileElement> driver;
+
+    public static void main(String[] args) throws MalformedURLException {
+
+        File classPathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classPathRoot, "/app/Android");
+        File app = new File(appDir, "Contacts.apk");
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Google Nexus");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, app);
+        desiredCapabilities.setCapability("appPackage", "com.jayway.contacts");
+        desiredCapabilities.setCapability("appActivity", "com.jayway.contacts.MainActivity");
+        desiredCapabilities.setCapability("newCommandTimeout", 300);
+
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(5, SECONDS);
+
+    }
+
 }
---
+++ SampleAppTests/src/test/java/waitings/Waitings.java
@@ -0,0 +1,21 @@
+package waitings;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.function.Function;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class Waitings {
+
+    public static void main(String[] args) {
+
+    }
+}
