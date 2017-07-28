f9212ca6cb3d9ca1a551ff63217eeda24b72603e
Eugeniy Petrik <eugeniy.petrik@gmail.com>
2017-07-28 21:36:23 +0300

    Add test cases in class

--- SampleAppTests/src/test/java/FirstTest.java
+++ SampleAppTests/src/test/java/FirstTest.java
@@ -3,11 +3,14 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -30,7 +33,36 @@ public class FirstTest {
         desiredCapabilities.setCapability("newCommandTimeout", 300);
 
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-        driver.manage().timeouts().implicitlyWait(5, SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, SECONDS);  // implicit wait
+
+        MobileElement searchField = driver.findElement(By.id("main_search"));
+        searchField.setValue("Joy S");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        List<MobileElement> name = driver.findElements(By.id("name"));
+        Assert.assertEquals(name.size(), 1);    // Assert.assertTrue(name.size() == 1);
+
+        for (MobileElement element : name) {
+            element.getText().equals("Joy Stclair");
+            element.click();
+        }
+
+        MobileElement image = driver.findElement(By.xpath("//*[@resource-id=\"android:id/content\"]//android.widget.ImageView"));
+
+        // driver.findElement(By.className("android.widget.ImageView"));
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
 
     }
 
